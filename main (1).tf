0f4629c2b7368fd0562ba40d8708a72b29977c9f
priyakumari91 <111412768+priyakumari91@users.noreply.github.com>
2023-06-16 11:22:26 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIA5BMGLEQ54AX2QAGF"
-  secret_key = "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"
-  region     = "ca-central-1"
+  access_key = "AKIAWQQ2VW7QMQFDPJSX"
+  secret_key = "FCC78Yhu1EhPMEk3BrGsCFUvULXO4EE0W3PbmEVL"
+  region     = "eu-west-3"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-08947f251e1b8f3b9"
+  ami           = "ami-00a215c1938e59989"
   instance_type = "t2.micro"
 
   tags = {
